9013c5204799e8906627281db34120e41ee99178
Felipe Messias <felipe.messias.silva@outlook.com>
2022-03-01 12:11:50 -0300

    push to github my project

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {v4 as uuidv4} from 'uuid';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import './App.css';
+import AddTask from './components/AddTask';
+import Tasks from './components/Tasks';
+import Header from './components/Header';
+import TaskDetails from './components/TaskDetails';
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true
+    },
+  ]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const { data } = await axios.get(['https://jsonplaceholder.cypress.io/todos?_limit=10']);
+      setTasks(data);
+    };
+
+    fetchTasks();
+  }, []);
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map(task => {
+      if (task.id === taskId) return {... task, completed: !task.completed}
+      return task;
+    });
+
+    setTasks(newTasks);
+  };
+
+  const handleTaskAddition = (tastTitle) => {
+    const newTasks = [
+      ...tasks, 
+      {
+      title: tastTitle,
+      id: uuidv4(),
+      completed: false
+    },
+  ];
+
+  setTasks(newTasks);
+  }
+
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId)
+
+    setTasks(newTasks)
+  };
+
+  return (
+    <Router>
+      <div className="container">
+        <Header />
+        <Route path="/" exact render={() => (
+          <>
+          <AddTask handleTaskAddition={handleTaskAddition} />
+            <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
+           </>
+        )}
+        />
+        
+        <Route path="/:taskTitle" exact component={TaskDetails}/>
+        </div>
+    </Router>
+  )
+}
+
+export default App;
